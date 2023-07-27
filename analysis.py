546260967fc6e54dc28116a1500f969dae21d81c
miku0 <miku.f.810129@gmail.com>
2023-07-27 01:38:47 +0000

    Number of addresses found by normal and not found by trigger

---
+++ analysis.py
@@ -0,0 +1,21 @@
+import csv
+# Number of addresses found by normal and not found by trigger
+set_found_normal = set()
+with open("project_counts.csv", 'r') as file:
+    reader = csv.reader(file)
+    data = list(reader)
+for i in range(len(data)):
+    set_found_normal.add(data[i][0])
+
+set_NOfound_trigger = set()
+with open("project2_zero.csv", 'r') as file:
+    reader = csv.reader(file)
+    data = list(reader)
+for i in range(len(data)):
+    set_NOfound_trigger.add(data[i][0])
+
+intersection_result = set_NOfound_trigger & set_found_normal
+with open("normal_found_trigger_NOfound.csv", 'w', newline='') as outfile:
+    writer = csv.writer(outfile)
+    for item in intersection_result:
+        writer.writerow([item])
